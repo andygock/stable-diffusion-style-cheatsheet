3825a066d25b01e2395090fefe1283dc3db3de8d
Andy Gock <andy@gock.net>
2024-01-17 00:51:37 +1100

    General update

--- scripts/generate_prompts_file.sh
+++ scripts/generate_prompts_file.sh
@@ -7,14 +7,16 @@
 #   cat data/prompts-female.txt | bash ./scripts/generate_prompts_file.sh -female > data/prompts-full/female.txt
 #   cat data/prompts-male.txt | bash ./scripts/generate_prompts_file.sh -male > data/prompts-full/male.txt
 #
+# Note: using "in bedroom" appears to generate a lot of nudity
+#
 
 arrFemale[0]="highest quality photo of 40yo brunette woman indoors in lounge room"
-arrFemale[1]="highest quality photo of 20yo blonde european woman indoors in bedroom"
+arrFemale[1]="highest quality photo of 20yo blonde european woman indoors at home"
 arrFemale[2]="highest quality photo of 30yo redhead american woman outdoors in city"
 arrFemale[3]="highest quality photo of 60yo blonde woman indoors relaxing at home"
 
 arrMale[0]="highest quality photo of 40yo european man indoors in lounge room"
-arrMale[1]="highest quality photo of 20yo asian man indoors in bedroom"
+arrMale[1]="highest quality photo of 20yo asian man indoors at home"
 arrMale[2]="highest quality photo of 30yo african american man outdoors in city"
 arrMale[3]="highest quality photo of 60yo nordic man indoors in workshop"
 
